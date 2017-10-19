3804c9dfad40e236ecc902ef8a8f910a8f27632d
Ivange Larry <ivangelarry@gmail.com>
2017-10-19 23:09:12 +0100

    Delete unused files

--- app/js/home/controllers/breadcrumbs.controller.js
+++
@@ -1,9 +0,0 @@
-class BreadCrumbsController {
-  constructor() {
-    var vm = this;
-    vm.links = {};
-    vm.links["Home Components"] = "";
-  }
-}
-
-export default BreadCrumbsController;
--- app/js/home/home.js
+++ app/js/home/home.js
@@ -1,15 +1,6 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
-import breadcrumbsComponent from './components/breadcrumbs.component.js';
-import breadcrumbsPatientCreateComponent from './components/breadcrumbsPatientCreate.component';
-import translateComponent from './components/translate.component.js';
 import headerComponent from './components/header.component.js';
-import notificationComponent from './components/notification.component.js';
-import patientSearchComponent from './components/patientSearch.component.js';
-import encounterComponent from './components/encounter.component.js';
-import observationComponent from './components/observation.component.js';
-import providerComponent from './components/provider.component.js';
-import patientCreateComponent from './components/patientCreate.component.js';
 import uicommons from 'openmrs-contrib-uicommons';
 
 let homeModule = angular.module('home', [ uiRouter, 'openmrs-contrib-uicommons'])
@@ -21,11 +12,6 @@ let homeModule = angular.module('home', [ uiRouter, 'openmrs-contrib-uicommons']
             url: '/',
             template: require('./home.html')
         });
-
-        $stateProvider.state('createPatient', {
-        url: '/create-patient',
-        template: require('./patientCreatePage.html')
-        })
     })
     .config(['$qProvider', function ($qProvider) {
       $qProvider.errorOnUnhandledRejections(false);
@@ -37,15 +23,6 @@ let homeModule = angular.module('home', [ uiRouter, 'openmrs-contrib-uicommons']
       $locationProvider.hashPrefix('');
     }])
 
-    .component('breadcrumbsComponent', breadcrumbsComponent)
-    .component('breadcrumbsPatientCreateComponent', breadcrumbsPatientCreateComponent)
-    .component('translateComponent', translateComponent)
-    .component('headerComponent', headerComponent)
-    .component('notificationComponent', notificationComponent)
-    .component('patientSearchComponent', patientSearchComponent )
-    .component('encounterComponent', encounterComponent)
-    .component('observationComponent', observationComponent)
-    .component('providerComponent', providerComponent)
-    .component('patientCreateComponent', patientCreateComponent );
+    .component('headerComponent', headerComponent);
 
 export default homeModule;
