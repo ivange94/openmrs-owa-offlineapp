cbbedc4162d6e3ff48a4c3fd0345bb5103803479
Ivange Larry <ivangelarry@gmail.com>
2017-10-30 13:57:25 +0100

    Search patient

--- app/js/home/components/patient-search/patientSearch.controller.js
+++ app/js/home/components/patient-search/patientSearch.controller.js
@@ -1,6 +1,24 @@
 class PatientSearchController {
-    constructor() {
+    constructor($http) {
         var vm = this;
+
+        vm.query = "";
+        vm.url = "/openmrs/ws/rest/v1/patient?v=full&q=";
+        vm.patients = [];
+        vm.hide = true;
+
+        vm.search = () => {
+            $http.get(vm.url + vm.query)
+                .then(function(resonse) {
+                    vm.patients = resonse.data.results;
+                    if (vm.patients.length > 0) {
+                        vm.hide = false;
+                    }
+                    else {
+                        vm.hide = true;
+                    }
+                });
+        }
     }
 }
 
