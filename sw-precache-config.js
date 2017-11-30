9b7b6f7e58533b4e2eed258d2991d3465b55beb1
Ivange Larry <ivangelarry@gmail.com>
2017-11-30 20:15:56 +0100

    Cache api get calls using networkFirst strategy

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -7,5 +7,11 @@ module.exports = {
         'dist/**.js',
         'dist/**.css',
         'dist/**.map'
+    ],
+    runtimeCaching: [
+        {
+            urlPattern: /^http:\/\/localhost:8080\/openmrs\/ws\/rest\/v1/,
+            handler: 'networkFirst'
+        }
     ]
 };
