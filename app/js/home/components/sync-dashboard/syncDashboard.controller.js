5b7900ba053ce2727ce3982abfb7b5e45ef5eb06
Ivange Larry <ivangelarry@gmail.com>
2018-01-07 16:05:14 +0100

    Sync data from sync dashbaord

--- app/js/home/components/sync-dashboard/syncDashboard.controller.js
+++ app/js/home/components/sync-dashboard/syncDashboard.controller.js
@@ -2,22 +2,40 @@ import localforage from "localforage";
 
 class SyncDashboardController {
     /* @ngInject */
-    constructor($http) {
+    constructor($http, $state) {
         var vm = this;
 
-        localforage.getItem("queue").then((queue) => {
-            vm.queue = queue;
-            console.log(vm.queue);
-        }).catch((err) => {
-            console.log(err);
-        });
-
-        vm.sync = (request) => {
-            console.log("Syncing..., ", request);
+        vm.sync = (request, index) => {
             $http.post(request.url, JSON.stringify(request.data)).then(response => {
-                console.log(result);
+                vm.queue.splice(index, 1);
+                updateOfflineStorage();
+                $state.go('patients', {patientId: response.data.uuid, patientCreated: true});
             }).catch(err => {
                 console.log("Error syncing.", err);
+            });
+        }
+
+        vm.cancel = (index) => {
+            vm.queue.splice(index, 1);
+            updateOfflineStorage();
+        }
+
+        vm.refresh = () => {
+            localforage.getItem("queue").then((queue) => {
+                vm.queue = queue;
+                console.log(vm.queue);
+            }).catch((err) => {
+                console.log(err);
+            });
+        }
+
+        vm.refresh();
+
+        var updateOfflineStorage = function() {
+            localforage.setItem("queue", vm.queue).then(queue => {
+                console.log("Deleted request from queue.", queue);
+            }).catch(err => {
+                console.log("Error deleting request from queue");
             });
         }
     }
